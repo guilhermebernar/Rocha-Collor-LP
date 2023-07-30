be2c428986459f12031d8c67076865b0e0bcee9c
guilhermebernar <guilhermebernardo@id.uff.br>
2023-07-30 00:38:31 -0300

    first commit

---
+++ src/components/ContactButton.js
@@ -0,0 +1,12 @@
+// components/ContactButton.js
+export default function ContactButton() {
+  return (
+    <a
+      href="https://wa.me/5521986368650"
+      target="_blank"
+      style={{ position: "fixed", bottom: "10px", right: "10px" }}
+    >
+      Entre em contato pelo WhatsApp agora!
+    </a>
+  );
+}
