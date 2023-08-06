e8c2b0da0a743cb5a3b1a03b793917146691f478
guilhermebernar <guilhermebernardo@id.uff.br>
2023-08-05 22:42:07 -0300

    feat: google tag maneger added correctly

--- src/components/GoogleTagManeger.js
+++ src/components/GoogleTagManeger.js
@@ -4,9 +4,13 @@ export default function GoogleTagManager() {
   return (
     <div>
       <Script
+        strategy="afterInteractive"
         src="https://www.googletagmanager.com/gtag/js?id=AW-713223450"
       />
-      <Script id="google-analytics">
+      <Script 
+        id="google-analytics"
+        strategy="afterInteractive"
+        >
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
